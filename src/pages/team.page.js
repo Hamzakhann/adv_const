d2427d835b5880035f2b996a77985a4b6e4830fe
Muhammad Maaz <m.jshah13@gmail.com>
2019-12-22 22:09:42 +0500

    team page mob view

--- src/pages/team.page.js
+++ src/pages/team.page.js
@@ -8,7 +8,15 @@ function TeamPage() {
     const [teams, setTeam] = useState();
     useEffect(() => {
         teamService.getAll().then(res => setTeam(res));
+       
     }, []);
+   
+    
+    
+        
+
+
+
     return (
         <div style={{ background: "#F7F7F7" }}>
             <HeaderComponent />
@@ -20,14 +28,14 @@ function TeamPage() {
                     { teams ? teams.map(team => (
 
                     <div key={team.id} className='row mt-3' >
-                        <div className='col-sm col-md-6' >
+                        <div className='col-sm-12 col-12 col-md-6' >
                             <div className='team-member-description bg-white p-4' >
                                 <h6 className='font-weight-bold who-we-are-heading' style={{fontSize:'20px'}} >{team.designation}</h6>
                                 <h3 className='font-weight-bold who-we-are-heading' style={{fontSize:'35px'}} >{team.name}</h3>
                                 <p>{team.description}</p>
                             </div>
                         </div>
-                        <div className='col-sm col-md-6' >
+                        <div className='col-sm-12 col-12 col-md-6 margin ' >
                             <div className='team-member-image' >
                                 <img className='img img-fluid' src={team.image} />
                             </div>
@@ -40,14 +48,14 @@ function TeamPage() {
             <section className='mb-n5' >
                 <div className="container p-0 mb-4">
                     <div className="row">
-                        <div className="col-md-6 col-sm-12" style={{marginTop: "20px"}}>
+                        <div className="col-md-6 col-sm-12 " style={{marginTop: "20px"}}>
                             <div className="inner"> 
                                 <h6>Quality</h6>
                                 <h3 className="h3-responsive who-we-are-heading">Delivering agile solutions to<br />complex projects</h3>
                                 <Link to="/quality">Learn about our work</Link>
                             </div>
                         </div>
-                        <div className="col-md-6 col-sm-12" style={{ marginTop: "20px"}}>
+                        <div className="col-md-6 col-sm-12 " style={{ marginTop: "20px"}}>
                         <img src="/quality_banner.jpg" className="img-fluid" />
                         </div>
                     </div>

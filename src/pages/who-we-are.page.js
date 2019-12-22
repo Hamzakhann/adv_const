9620d8c4993f44c1a9e35c1776f7b665b78ffb2e
Muhammad Maaz <m.jshah13@gmail.com>
2019-12-22 21:05:11 +0500

    who-we-are page responsive

--- src/pages/who-we-are.page.js
+++ src/pages/who-we-are.page.js
@@ -25,7 +25,7 @@ function WhoWeArePage() {
 								</div>
 								<br />
 								<div className='row' >
-									<div className='col-sm col-md-10' >
+									<div className='col-sm-12 col-12 col-md-10' >
 									<div className='who-we-are-content-edit'  dangerouslySetInnerHTML={{__html:whoWeAre.primary_description}}>
 								</div>
 									</div>
@@ -35,12 +35,15 @@ function WhoWeArePage() {
 						</div>
 						<div className="row">
 							<div className="col-md-5 col-sm-12  tex-box" style={{ marginTop: "50px" }}>
+							<div className="col-md-7 col-sm-12 mobile-work-img " style={{ marginTop: "50px" }}>
+								<img src={whoWeAre.ceo_message_image} className="img-fluid" />
+							</div>
 								<div className="inner" >
 									<h3 className='who-we-are-heading font-weight-bold' style={{ paddingRight: '40px', fontSize: '34px' }} >{whoWeAre.ceo_message}</h3>
 									<a href="#">CEO</a>
 								</div>
 							</div>
-							<div className="col-md-7 col-sm-12 p-0" style={{ marginTop: "50px" }}>
+							<div className="col-md-7 col-sm-12 desktop-work-img " style={{ marginTop: "50px" }}>
 								<img src={whoWeAre.ceo_message_image} className="img-fluid" />
 							</div>
 						</div>
@@ -48,7 +51,7 @@ function WhoWeArePage() {
 						<div className="row contactUs-text-container">
 							<div className="col-md-12 col-sm-12">
 								<h3 className="who-h1">{whoWeAre.secondary_title}</h3>
-								<p className='font-weight-bold who-we-are-heading' style={{ lineHeight: '2em', paddingRight: '100px' }} >
+								<p className='font-weight-bold who-we-are-heading ' style={{ lineHeight: '2em', paddingRight: '100px' }} >
 									{whoWeAre.secondary_description}
 								</p>
 
@@ -56,16 +59,19 @@ function WhoWeArePage() {
 						</div>
 
 						<div className="row mb-5">
-							<div className="col-md-6 col-sm-12 box" style={{ marginTop: "100px" }}>
+							<div className="col-md-6 col-sm-12 box desktop-work-img" style={{ marginTop: "100px" }}>
 								<img src={whoWeAre.work_with_us_image} className="img-fluid" />
 							</div>
 
-							<div className="col-md-6 col-sm-12 p-0" style={{ marginTop: "100px" }}>
+							<div className="col-md-6 col-sm-12 col-12 p-0 work" style={{ marginTop: "100px" }}>
 								<div className="inner">
-									<h6 className='pl-5' >Work With Us</h6>
-									<h3 className="h3-responsive font-weight-bold pl-5 pr-5">{whoWeAre.work_with_us_title}</h3>
-									<Link to="/work-at-acc" className='pl-5 mt-3' >Apply now</Link>
+									<h6 className='' >Work With Us</h6>
+									<h3 className="h3-responsive font-weight-bold ">{whoWeAre.work_with_us_title}</h3>
+									<Link to="/work-at-acc" className='' >Apply now</Link>
 								</div>
+								<div className="col-md-6 col-sm-12 box mobile-work-img" >
+            <img src="/banner2.jpg" className="img-fluid" />    
+          </div>
 							</div>
 						</div>
 

d3fc5a1ac9ccd59a79cb9a93842e369145bf6b6b
Hamzakhann <hamzakhann66@gmail.com>
2020-02-09 14:57:31 +0500

    uncommit build folder

---
+++ build/precache-manifest.69c0b98625a581a414f9ea8f66545565.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aaea2af9886fe79d230015b194b8b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312dfe42c54e8ce1e098",
+    "url": "/static/css/2.52c9f559.chunk.css"
+  },
+  {
+    "revision": "5a34bb7c69608a7efa7a",
+    "url": "/static/css/main.3e3f4d63.chunk.css"
+  },
+  {
+    "revision": "312dfe42c54e8ce1e098",
+    "url": "/static/js/2.335457c5.chunk.js"
+  },
+  {
+    "revision": "5c9046d5940c5281fdbc53287637ff4d",
+    "url": "/static/js/2.335457c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a34bb7c69608a7efa7a",
+    "url": "/static/js/main.aaaf7854.chunk.js"
+  },
+  {
+    "revision": "f26b9d31212a217f2263",
+    "url": "/static/js/runtime-main.572fca3e.js"
+  },
+  {
+    "revision": "7088ba8ac7367ece28f1968df884b522",
+    "url": "/static/media/FoundersGroteskWeb-Regular.7088ba8a.eot"
+  },
+  {
+    "revision": "88c9afce434b7d366dd34a04cc202973",
+    "url": "/static/media/FoundersGroteskWeb-Regular.88c9afce.woff2"
+  },
+  {
+    "revision": "de386890c4543cd1e9ac35273f94f870",
+    "url": "/static/media/FoundersGroteskWeb-Regular.de386890.woff"
+  },
+  {
+    "revision": "7f78807032d2100063e2f5c43c1d1795",
+    "url": "/static/media/NationalWeb-Medium.7f788070.woff2"
+  },
+  {
+    "revision": "9c89484d38bf015743e7d5fc8eb14f91",
+    "url": "/static/media/NationalWeb-Medium.9c89484d.woff"
+  },
+  {
+    "revision": "d363c75a88b954640f8288794942dc09",
+    "url": "/static/media/NationalWeb-Medium.d363c75a.eot"
+  }
+]);
